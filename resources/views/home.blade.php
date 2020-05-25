7eee05814c3e037c80b1f54f483a179639fdc417
MiltonAguirre <milton.aguirre92@gmail.com>
2020-05-25 03:19:01 -0300

    1 - unfinished home view and bar customization

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,19 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+  <!-- Masthead-->
+  <header class="masthead">
+      <div class="container">
+        <div class="masthead-heading">Bienvenido a BarberON</div>
+          <div class="masthead-subheading">Busca tu barberia favorita y obtene tu turno</div>
+          <form class="form-inline form-inline-search">
+              <input type="text" class="form-control" size="50" name="barber_name" placeholder="Nombre de la barberia">
+              <div class="input-group-btn">
+                <button type="submit" class="btn btn-primary">Buscar</button>
+              </div>
+          </form>
+      </div>
+  </header>
+  
 </div>
 @endsection
