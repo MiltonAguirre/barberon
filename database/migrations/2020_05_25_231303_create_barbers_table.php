f37c1c8478e61b21b6f1c694f29f2d357a12399f
MiltonAguirre <milton.aguirre92@gmail.com>
2020-05-25 20:54:12 -0300

    1 - migrate products improved

--- database/migrations/2020_05_25_231303_create_barbers_table.php
+++ database/migrations/2020_05_25_231303_create_barbers_table.php
@@ -14,7 +14,7 @@ class CreateBarbersTable extends Migration
     public function up()
     {
         Schema::create('barbers', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('name');
             $table->string('phone');
             $table->unsignedInteger('user_id');
